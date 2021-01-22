217b8c385409d2d51ad3dacedb9cbffe1b5dd0c2
Alver Lopez <alver@giganovus.com>
2021-01-22 00:09:08 -0400

    Parameter renaming.

--- gpscraper/forms.py
+++ gpscraper/forms.py
@@ -11,7 +11,7 @@ class Sort:
         return value in [cls.MOST_RELEVANT, cls.NEWEST, cls.RATING]
 
 
-def reviews_next_page(id, next_page_token, review_size, sort_type, sort_score):
+def reviews_next_page(id, next_page_token, review_size, sort_type, score):
     if not next_page_token:
         next_page_token = json.dumps(None)
     else:
@@ -19,15 +19,15 @@ def reviews_next_page(id, next_page_token, review_size, sort_type, sort_score):
 
     sort_type = json.dumps(sort_type)
 
-    if 1 <= sort_score <= 5:
-        sort_score = f'[null,{sort_score}]'
+    if 1 <= score <= 5:
+        score = f'[null,{score}]'
     else:
-        # By default, Google Play sorts by all score.
-        sort_score = '[]'
+        # By default, Google Play shows reviews of all scores.
+        score = '[]'
 
     long_data = (
         f'[null,null,[2,{sort_type},[{review_size},null,'
-        f'{next_page_token}],null,{sort_score}],["{id}",7]]'
+        f'{next_page_token}],null,{score}],["{id}",7]]'
     )
 
     form = {
