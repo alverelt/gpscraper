65fa686ea3c13a07d960e8ada0f13487b806aa64
Alver Lopez <alver@giganovus.com>
2021-03-06 20:59:01 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="gpscraper",
-    version="1.0.0",
+    version="1.1.0",
     description="A nice Google Play scraper.",
     long_description=README,
     long_description_content_type="text/markdown",
